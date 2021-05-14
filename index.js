ff2e72eca3752aaf3e0e234532e6b381ab94961e
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-14 04:05:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// add solution here
+function theBeatlesPlay(musician,instruments)
+{//musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+//instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+//console.log (musicians,instruments)
+let arr=[]
+for (let i=0; i < musicians.length, i++){
+  const string= musicians[i] + "plays "+instruments[i];
+  arr.push(str);
+}
+return arr;
+}
