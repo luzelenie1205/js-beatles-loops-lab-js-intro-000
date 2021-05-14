eb1cdd3f321e4e8c225aa6cd1af25e80a6e65222
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-14 04:08:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ function theBeatlesPlay(musician,instruments)
 {
 musicians = [];
 instruments = [];
-console.log (musicians,instruments)
-let arr=[]
-for (let i=0; i < musicians.length, i++){
-  const string= musicians[i] + "plays "+instruments[i];
-  arr.push(str);
-}
-return arr;
+
+
+
 }
