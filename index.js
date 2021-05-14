0b891c61e3010ade7de98037d15443f2ca47624c
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-14 04:06:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musician,instruments)
-{//musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-//instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-//console.log (musicians,instruments)
+{musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+console.log (musicians,instruments)
 let arr=[]
 for (let i=0; i < musicians.length, i++){
   const string= musicians[i] + "plays "+instruments[i];
