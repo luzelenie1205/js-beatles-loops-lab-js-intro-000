318328ddb8f85fab604da3654ea7359488ba0030
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-14 04:15:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
+// function theBeatlesPlay(musicians, instruments) {
+//   var array = [];
+//   for(var i = 0; i < musicians.length; i++) {
+//     array.push(`${musicians[i]} plays ${instruments[i]}`);
+//   }
+//   return array;
+// }
+//
+// function johnLennonFacts(array) {
+//   var i = 0;
+//   while (i < array.length) {
+//     array[i] = array[i] + "!!!";
+//     i++;
+//   }
+//   return array;
+// }
+//
+// function iLoveTheBeatles(number) {
+//   var array = [];
+//   do {
+//     array.push('I love the Beatles!');
+//     number ++;
+//   } while (number < 15);
+//   return array;
+// }
 
-
-function theBeatlesPlay(musician,instruments)
-{
-  musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-  
-var arr=[];
-for (let i=0; i < musicians.length, i++){
-console.log( musicians[i] + "plays "+instruments[i]);
-//arr.push(str);
+function theBeatlesPlay(musicians,instruments) {
+  var x = []
+  for (var i = 0; i < musicians.length; i++) {
+    x.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return x;
 }
-return arr;
+
+function johnLennonFacts(array) {
+  var i = 0
+  while (i < array.length) {
+    array[i] += '!!!';
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(number) {
+  var arr = [];
+  var i = 0;
+  do {
+    arr.push('I love the Beatles!');
+    i++;
+  } while (i <= number && number < 15);
+  return arr;
 }
